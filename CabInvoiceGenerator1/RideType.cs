1ebe2f5d9c3f0c15b51e9841ccb71ca51561ec48
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-11 22:57:14 +0530

    [Ravina] Added : Defining the ride type i. e. Normal and Premium

---
+++ CabInvoiceGenerator1/RideType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator1
+{
+    public enum RideType
+    {
+        NORMAL,
+        PREMIUM
+
+    }
+}
